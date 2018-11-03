c5ce1257ad0f1c0f925ddb10fe55b0cfdabbcb81
Eugene Mihailenco <mihailencoe@gmail.com>
2018-11-04 00:34:57 +0200

    Cleanup: Fixed C++ version, higher stakes with 100m elem vectors

--- random_array_dot.cpp
+++ random_array_dot.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
-#include <memory>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <unordered_map>
+#include <iomanip>
+// #include <memory>
+// #include <algorithm>
+// #include <vector>
+// #include <list>
+// #include <unordered_map>
 
 #include <chrono>  // for high_resolution_clock
 
@@ -16,19 +17,27 @@ using namespace std;
 using namespace std::chrono;
 using namespace Eigen;
 
-typedef Matrix<double,10000000,1> ArrayRandy;
+constexpr int DIM_SIZE = 100'000'000;
+
+//typedef Array<double,10000000,1> LongArray; // Won't work - will try to allocate on stack
+
 
 int main()
 {
 
-    auto x = ArrayRandy::Random();
-    auto y = ArrayRandy::Random();
+    // Holy crap, this is so Ugly, really no better Eagen way to do his?
+    ArrayXd x(DIM_SIZE);
+    ArrayXd y(DIM_SIZE);
+    x.setRandom();
+    y.setRandom();
+    x = (x + 1.0) * 0.5;
+    y = (y + 1.0) * 0.5;
 
     auto start = high_resolution_clock::now();
-    auto v = x.dot(y);
+    double v = x.cwiseProduct(y).sum(); // Srsly? no dot product for Array, invent your own?
     auto finish = high_resolution_clock::now();
 
-    cout << "===============\n" << v  << endl;
+    cout << "===============\n" << std::fixed << std::setprecision(3) << v  << endl;
     std::cout << "Calculation took "
               << duration_cast<milliseconds>(finish - start).count()
               << "ms.\n";
